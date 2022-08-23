8bb7c1894f13fce5f57704fd0fdd40e1204a5353
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-23 16:39:10 +0300

    message repo created

---
+++ backend/src/main/java/com/gucardev/backend/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.gucardev.backend.repository;
+
+import com.gucardev.backend.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findAllByRoom(String room);
+}
