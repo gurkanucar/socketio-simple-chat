136870da1ea5e889eb1b804970700d5feeacf1ba
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-23 16:37:29 +0300

    message constants created

---
+++ backend/src/main/java/com/gucardev/backend/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.gucardev.backend.constants;
+
+public class Constants {
+
+    public static final String WELCOME_MESSAGE = "%s joined to chat";
+    public static final String DISCONNECT_MESSAGE = "%s disconnected";
+
+}
