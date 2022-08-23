f0266be713db7f718d8f3eb50d96e15f2e046e72
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-23 16:39:28 +0300

    message service created

---
+++ backend/src/main/java/com/gucardev/backend/service/MessageService.java
@@ -0,0 +1,27 @@
+package com.gucardev.backend.service;
+
+import com.gucardev.backend.model.Message;
+import com.gucardev.backend.repository.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+
+    public List<Message> getMessages(String room) {
+        return messageRepository.findAllByRoom(room);
+    }
+
+    public Message saveMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
+}
