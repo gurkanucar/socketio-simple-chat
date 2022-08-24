0561c5f447dbeb89cf5efe306391eab6c5d50b88
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-24 10:37:23 +0300

    use fetch file name updated

---
+++ frontend/src/customHooks/useFetch.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "../constants/apiConstants";
+
+export const useFetch = (url) => {
+  const [responseData, setResponseData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        setLoading(true);
+        const response = await axios.get(API_BASE_URL + url);
+        setResponseData(response.data);
+        console.log(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { responseData, error, loading };
+};
